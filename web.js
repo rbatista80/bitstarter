f55b775ac160e5808a6f3727891dc84dcd995ecd
Ricardo Batista <ricardo.batista@gmail.com>
2013-07-15 02:42:03 +0000

    Change to get text from index.html

--- web.js
+++ web.js
@@ -2,8 +2,20 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var textinfo = new Buffer('');
+
+fs.readFile('index.html','utf-8', function(err,data) {
+    if (err) throw err;
+    console.log(data);
+    textinfo.write(data);
+});
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(textinfo);
 });
 
 var port = process.env.PORT || 5000;
